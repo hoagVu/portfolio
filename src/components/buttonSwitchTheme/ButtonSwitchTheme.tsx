40a9b7a12d33f515471c918a6eab0eadcf57515c
hoangVu.dev <hoangk62uet@gmail.com>
2023-06-09 01:14:31 +0700

    update button switch theme

---
+++ src/components/buttonSwitchTheme/ButtonSwitchTheme.tsx
@@ -0,0 +1,27 @@
+import useDarkMode from "@/hooks/useDarkMode";
+import * as React from "react";
+import "./ButtonSwitchTheme.scss";
+
+interface IButtonSwitchThemeProps {}
+
+const ButtonSwitchTheme: React.FunctionComponent<IButtonSwitchThemeProps> = (
+  props
+) => {
+  const { toggleDarkMode, currentTheme } = useDarkMode();
+
+  return (
+    <div className="btn-switch-theme">
+      <input
+        type="checkbox"
+        id="checkbox"
+        onClick={toggleDarkMode}
+        data-theme={currentTheme}
+      />
+      <label htmlFor="checkbox" className="switch">
+        <div className="powersign"></div>
+      </label>
+    </div>
+  );
+};
+
+export default ButtonSwitchTheme;
